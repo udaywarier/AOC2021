89566807672ea0a27fd2725686c816da15a3c0d2
udaywarier <uday.warier1@gmail.com>
2022-10-29 00:35:27 -0400

    Fixing create script to add input and test txt files, adding up to Day13

--- create_folder.py
+++ create_folder.py
@@ -14,7 +14,7 @@ os.system('mkdir ' + next_folder_name)
 
 # Set the name of the starter file and its contents.
 starter_file_name = next_folder_name + '/soln.py'
-starter_file_contents = '# Read the contents of the input into a list\ninput = []\nwith open(\'./input.txt\', \'r\') as f:\n\tinput = [line.strip() for line in f.readlines()]'
+starter_file_contents = '# Read the contents of the input into a list\ninput = []\nwith open(\'./input.txt\', \'r\') as f:\n\tinput = [line.strip() for line in f.readlines()]\n'
 
 # Create the starter file and write the given contents.
 with open(starter_file_name, 'w') as fd:
@@ -25,3 +25,9 @@ input_file_name = next_folder_name + '/input.txt'
 input_file_content = ''
 with open(input_file_name, 'w') as ifd:
     ifd.write(input_file_content)
+
+# Create a blank .txt file for the test input.
+test_file_name = next_folder_name + '/test.txt'
+test_file_content = ''
+with open(test_file_name, 'w') as tfd:
+    tfd.write(test_file_content)    
